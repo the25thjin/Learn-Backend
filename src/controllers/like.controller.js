4599789d1abcc49c82fc8f1dd1f5f24ea593518d
the25thjin <mannr075@gmail.com>
2024-07-29 18:11:55 +0530

    fixed minor bug in like controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,6 +1,6 @@
 import { Like } from "../models/like.models.js";
 import { Comment } from "../models/comment.models.js";
-import { User } from "../models/user.models.js";
+import { Tweet } from "../models/tweet.models.js";
 import { Video } from "../models/video.models.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
