856924c6d590c0b2a5c4dba499a9d9e6fb4c8736
the25thjin <mannr075@gmail.com>
2024-07-31 11:35:23 +0530

    added route in app.js

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ import commentRouter from "./routes/comment.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import subcriptionRouter from "./routes/subscription.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
 //routes declaration
 app.use("/api/v1/users",userRouter)
 app.use("/api/v1/videos",videoRouter)
@@ -30,5 +31,6 @@ app.use("/api/v1/comments",commentRouter)
 app.use("/api/v1/tweets",tweetRouter)
 app.use("/api/v1/likes",likeRouter)
 app.use("/api/v1/subscriptions",subcriptionRouter)
+app.use("/api/v1/playlists",playlistRouter)
 
 export {app}
